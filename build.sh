27987b036a42776c7672e543031405d1b2147f3c
garywill <garywill@disroot.org>
2025-09-20 20:00:00 +0800

    update build script

--- build.sh
+++ build.sh
@@ -3,5 +3,5 @@
 mkdir -p dist
 rm dist/* 
 ./include.py > dist/taskmonitor_part1.uc.js
-cp src/taskmonitor_part2.uc.js dist/
+cp src/taskmonitor_part2.uc.js.sys.mjs dist/
 cp src/taskmonitor_part3_clearMemoryPeriodically.uc.js dist/taskmonitor_part3_clearMemoryPeriodically.uc.js
